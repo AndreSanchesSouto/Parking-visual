d84a04db8373cd5c8faf1b7bad43cb6bfc6a2c49
André Sanches Souto <andresanchessouto@gmail.com>
2024-06-26 17:50:30 -0300

    register page

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return(
-        <header className="w-screen bg-indigo-600 bg-opacity-85 flex align-middle h-12 font-semibold text-white justify-between p-10">
-            <div className="self-center flex gap-10 text-2xl ">
+        <header className="w-screen bg-indigo-800 bg-opacity-90 flex align-middle h-12 font-semibold text-white justify-between p-10">
+            <div className="self-center flex gap-10 text-2xl">
                 <img src="" alt="" />
-                <Link to="/">Home</Link>
-                <Link to="/cars">Carros</Link>
-                <Link to="/car-register">Registrar Carro</Link>
-                <Link to="/drivers">Motorista</Link>
-                <Link to="/driver-register">Registrar Motorista</Link>
+                <Link to="/" className="hover:underline hover:text-indigo-">Home</Link>
+                <Link to="/cars" className="hover:underline">Carros</Link>
+                <Link to="/car-register" className="hover:underline">Registrar Carro</Link>
+                <Link to="/drivers" className="hover:underline">Motorista</Link>
+                <Link to="/driver-register" className="hover:underline">Registrar Motorista</Link>
             </div>
             <div className="flex self-center gap-10 text-xl ">
-                <Link to="/register">Cadastrar</Link>
-                <Link to="/login">Entrar</Link>
+                <Link to="/register" className="hover:underline">Cadastrar</Link>
+                <Link to="/login" className="hover:underline">Entrar</Link>
             </div>
         </header>
     );
