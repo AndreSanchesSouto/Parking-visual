43de78a94bb54609638630fe10d911b33e433892
André Sanches Souto <andresanchessouto@gmail.com>
2024-06-27 14:39:00 -0300

    update register page

--- src/pages/ValetRegister/ValetRegister.tsx
+++ src/pages/ValetRegister/ValetRegister.tsx
@@ -11,25 +11,6 @@ function Register() {
     const cellphoneRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
     const confirmPassRef = useRef<HTMLInputElement>(null)
-    
-    function secondPage(event: FormEvent){
-        event.preventDefault()
-        console.log(
-            nameRef.current?.value,
-            surnameRef.current?.value
-        )
-
-    }
-
-    function thirdPage(event: FormEvent) {
-        event.preventDefault()
-        console.log(
-            cpfRef.current?.value,
-            emailRef.current?.value,
-            cellphoneRef.current?.value
-        )
-
-    }
 
     function concatName(fisrtName: string, surname: string) {
         return fisrtName + " " + surname
@@ -64,21 +45,22 @@ function Register() {
     <>
         <Header/>
         <div className="bg-indigo-90 w-screen h-screen bg-[url('../components/img.jpg')] bg-cover flex justify-center items-center">
-            <div>
-                <div className="max-w-4xl bg-indigo-950 bg-opacity-95 shadow-2xl rounded-2xl text-white py-5 flex justify-around align-middle">
+            <form 
+            className="flex flex-col justify-around"
+            onSubmit={ handleRegisterValet }>
+                <div className=" bg-indigo-950 w-full bg-opacity-95 shadow-2xl gap-20 py-10 px-20 rounded-2xl text-white p-5 flex justify-around align-middle">
                     <div className="flex flex-col justify-center max-w-sm">
                         <h1 
-                        className="font-bold text-4xl"
+                        className="font-bold text-5xl"
                         >
                             Cadastro de usuário
                         </h1>
-                        <h2 className="text-sm">
+                        <h2 className="text-md">
                             Responda o que se pede para realizar o cadastro no sistema, para mais informações clique <a href=" "className="text-purple-400">aqui...</a>
                         </h2>
                     </div>
-                    <div className="flex ">
-                        <form className="flex flex-col max-w-xs gap-5 text-lg"
-                        onSubmit={ secondPage }
+                    <div className="flex">
+                        <div className="flex flex-col gap-5 text-lg"
                         >
                             <label className="flex flex-col gap-1">
                                 <span 
@@ -88,7 +70,9 @@ function Register() {
                                 </span>
                                 <input 
                                 type="text" 
-                                className="rounded-lg px-3 py-1 text-black bg-stone-300" 
+                                className="rounded-lg px-3 py-1 text-black bg-stone-300 text-sm" 
+                                placeholder="Primeiro nome"
+                                size={25}
                                 required
                                 ref={ nameRef }
                                 />
@@ -100,35 +84,29 @@ function Register() {
                                 </span>
                                 <input 
                                 type="text" 
-                                className="rounded-lg px-3 py-1 text-black bg-stone-300" 
+                                className="rounded-lg px-3 py-1 text-black bg-stone-300 text-sm" 
+                                placeholder="Sobrenome"
+                                size={25}
                                 required
                                 ref={ surnameRef }
                                 />
                             </label>
-                            <div className="w-full flex justify-end">
-
-                                <input 
-                                type="submit" 
-                                value="Avançar" 
-                                className="text-base w-max hover:text-yellow-400"
-                                />
-                            </div>
-                        </form>
+                        </div>
                     </div>  
                 </div>
-                <div className="m-auto max-w-4xl mt-5 bg-indigo-950 bg-opacity-95 shadow-2xl rounded-2xl text-white p-5 ">
+                <div className="mt-5 bg-indigo-950 bg-opacity-95 shadow-2xl rounded-2xl text-white p-5 w-full">
                     <div className="w-full flex gap-5 flex-col">
-                        <h2 className="text-2xl font-semibold text-center mb-3">Informações para contato</h2>
-                        <form 
-                        className="w-max text-lg"
-                        onSubmit={ thirdPage }
+                        <h2 className="text-3xl font-semibold text-center">Informações para contato</h2>
+                        <div 
+                        className=" text-lg"
                         >
-                            <div className="flex gap-x-7 gap-y-3 m-auto ">
+                            <div className="flex gap-x-7 gap-y-3 justify-center">
                                 <div className="flex flex-col">
                                     <label className="px-3 py-1">CPF</label>
                                     <input 
                                     type="text" 
-                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-lg"
+                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-sm"
+                                    placeholder="XXX.XXX.XXX-XX"
                                     required
                                     ref={ cpfRef }
                                     />              
@@ -137,7 +115,9 @@ function Register() {
                                     <label className="px-3 py-1">Email</label>
                                     <input 
                                     type="text" 
-                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-lg"
+                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-sm"
+                                    placeholder="seuemail@mail.com"
+                                    size={30}
                                     ref={ emailRef }
                                     />              
                                 </div>  
@@ -145,55 +125,56 @@ function Register() {
                                     <label className="px-3 py-1">Telefone</label>
                                     <input 
                                     type="text" 
-                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-lg"
+                                    className="rounded-lg px-3 py-1 text-black bg-stone-300 text-sm"
+                                    placeholder="(xx)9.xxxx-xxxx"
                                     ref={ cellphoneRef }
                                     />              
                                 </div>  
                                     
-                            </div> 
-                            <div className="w-full flex pt-4 shrink-0 justify-end">
-                                <button className="text-base w-max hover:text-yellow-400"
-                                >
-                                    Avançar
-                                </button>
-                            </div>            
-                        </form>
+                            </div>           
+                        </div>
                     </div>
                 </div>
-                <div className="m-auto max-w-4xl mt-5 bg-indigo-950 bg-opacity-95 shadow-2xl rounded-2xl text-white p-5 ">
-                    <form 
-                    className="grid grid-cols-2 gap-5"
+                <div className="m-auto mt-5 bg-indigo-950 bg-opacity-95 shadow-2xl rounded-2xl text-white p-5 w-full">
+                    <div 
+                    className=""
                     onSubmit={ handleRegisterValet }
                     >
-                        <div className="m-auto">
-                            <label className="flex flex-row px-3 mb-2 text-xl">Senha</label>
-                            <input 
-                            type="password" 
-                            className="px-3 py-1 rounded-lg text-black bg-stone-300"
-                            required
-                            ref={ passwordRef }
-                            />
-                        </div>
-                        <div className="m-auto">
-                            <label className="flex flex-row px-3 mb-2 text-xl">Confirmar Senha</label>
-                            <input 
-                            type="password" 
-                            className="px-3 py-1 rounded-lg text-black bg-stone-300"
-                            required
-                            ref={ confirmPassRef }
+                        <div className="flex justify-around">
+                            <div>
+                                <label className="flex px-3 mb-2 text-xl">Senha</label>
+                                <input 
+                                type="password" 
+                                className="px-3 py-1 rounded-lg text-black bg-stone-300 text-sm"
+                                placeholder="senha"
+                                required
+                                size={25}
+                                ref={ passwordRef }
+                                />
+                            </div>
+                            <div>
+                                <label className="flex px-3 mb-2 text-xl">Confirmar Senha</label>
+                                <input 
+                                type="password" 
+                                className="px-3 py-1 rounded-lg text-black bg-stone-300 text-sm"
+                                placeholder="confirmar senha"
+                                required
+                                size={25}
+                                ref={ confirmPassRef }
 
-                            />
+                                />
+                            </div>
                         </div>
-                        <div className="col-span-2 m-auto">
+                        <div className="flex justify-center">
                             <input 
                             type="submit" 
                             value="Cadastrar" 
-                            className="bg-orange-400 shadow-lg font-bold rounded-xl text-indigo-950 p-2 hover:duration-100 hover:bg-green-400 hover:"
+                            className="bg-orange-400 shadow-lg font-bold rounded-xl text-indigo-950 p-2 hover:duration-100 hover:bg-green-400 text-sm"
                             />
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </>
     );
